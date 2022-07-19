ab1dbb9eb6999a0d4aa4a029e5dfb837e35715b9
ShamimAhamed-prog <shamim.aiub2@gmail.com>
2022-07-19 10:48:58 +0600

    candlestick charts done with api

---
+++ app/Models/Stock.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Stock extends Model
+{
+    use HasFactory;
+    protected $table = 'stocks';
+    protected $fillable = ['date','open','high','low','close','volume_btc','volume_usd'];
+}
---
+++ database/migrations/2022_07_19_040700_create_stocks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->string('open');
+            $table->string('high');
+            $table->string('low');
+            $table->string('close');
+            $table->string('volume_btc');
+            $table->string('volume_usd');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stocks');
+    }
+}
