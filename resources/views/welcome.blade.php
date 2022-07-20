71a07e3dea85981dcbc3bde68c835fec75c16042
ShamimAhamed-prog <shamim.aiub2@gmail.com>
2022-07-20 17:27:42 +0600

    trading view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -96,5 +96,39 @@ window.onload = function () {
 </head>
 <body>
 <div id="chartContainer" style="height: 500px; width: 100%;"></div>
+//////////////////////////////tradingView///////////////////////////////////
+<div>
+  <!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container">
+  <div id="tradingview_873f9"></div>
+  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/NASDAQ-AAPL/" rel="noopener" target="_blank"><span class="blue-text">AAPL Chart</span></a> by TradingView</div>
+  <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+  <script type="text/javascript">
+  new TradingView.widget(
+  {
+  "autosize": true,
+  "symbol": "NASDAQ:AAPL",
+  "timezone": "UTC +6",
+  "theme": "light",
+  "style": "1",
+  "locale": "en",
+  "toolbar_bg": "#f1f3f6",
+  "enable_publishing": true,
+  "withdateranges": true,
+  "range": "YTD",
+  "hide_side_toolbar": true,
+  "allow_symbol_change": true,
+  "details": true,
+  "hotlist": true,
+  "show_popup_button": true,
+  "popup_width": "1000",
+  "popup_height": "650",
+  "container_id": "tradingview_873f9"
+}
+  );
+  </script>
+</div>
+<!-- TradingView Widget END -->
+</div>
 </body>
 </html>
